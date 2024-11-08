8d7294f49ef3f5c8be57ccfd7f181a798a6898fa
egaube <egaube@MacBook-Pro-de-egaube.local>
2024-11-08 18:00:41 +0100

    fix: fix all automatically detectable accessibility issues

--- e2e/kanban/kanban.spec.ts
+++ e2e/kanban/kanban.spec.ts
@@ -1,16 +1,15 @@
-import { expect, test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 import AxeBuilder from "@axe-core/playwright";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("/");
-  const firstProjectLink = page.locator("css=.mat-mdc-card").nth(0).getByRole("link");
+  const firstProjectLink = page.locator("css=.mat-mdc-card").nth(1).getByRole("link");
   await expect(firstProjectLink).toBeVisible();
   await firstProjectLink.click();
-  await page.getByTestId("settings-link").click();
-  await expect(page.locator("button[type=submit]")).toBeVisible();
+  await expect(page.locator(".kanban-viewport")).toBeVisible();
 });
 
-test.describe("Workspace settings", () => {
+test.describe("Kanban", () => {
   test("should not have any automatically detectable accessibility issues", async ({ page }) => {
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
--- e2e/signup/signup.spec.ts
+++ e2e/signup/signup.spec.ts
@@ -3,7 +3,6 @@ import AxeBuilder from "@axe-core/playwright";
 
 test.beforeEach("Load Email signup form", async ({ page }) => {
   await page.goto("/signup");
-  await expect(page.getByTestId("allOptions-div")).toBeVisible();
   await expect(page.getByTestId("showEmailSignupForm-button")).toBeVisible();
   await page.getByTestId("showEmailSignupForm-button").click();
   await expect(page.getByTestId("fullName-input")).toBeVisible();
@@ -26,10 +25,6 @@ test.describe("Signup", () => {
     await page.getByTestId("submitCreateAccount-button").click();
     await expect(page.getByTestId("sentEmail-block")).toContainText(emailStr);
     await expect(page.getByTestId("resendMail-button")).toBeVisible();
-  });
-  test("All options button returns to options page.", async ({ page }) => {
-    await page.getByTestId("allOptions-button").click();
-    await expect(page.getByTestId("allOptions-div")).toBeVisible();
   });
   test("Should display required fullName error", async ({ page }) => {
     await page.getByTestId("email-input").fill("valid@ourmail.com");
