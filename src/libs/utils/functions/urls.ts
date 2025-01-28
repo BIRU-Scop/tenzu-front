0f6097d7eabd16ebc2e3ffa264c533e232f0d5f0
paulguichon <pguichon@amarena.io>
2025-01-28 19:02:28 +0100

    feat(urls): add functions to generate story detail URLs
    Introduce `getStoryDetailListUrl` and `getStoryDetailUrl` functions for constructing URLs to story detail pages. Enhances URL generation utility for better reusability and consistency.

--- src/libs/utils/functions/urls.ts
+++ src/libs/utils/functions/urls.ts
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2025 BIRU
+ *
+ * This file is part of Tenzu.
+ *
+ * Tenzu is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can contact BIRU at ask@biru.sh
+ *
+ */
+
 import { ProjectSummary } from "@tenzu/data/project";
 import { Workflow } from "@tenzu/data/workflow";
 import { WorkspaceProject } from "@tenzu/data/workspace";
@@ -18,4 +39,12 @@ export function getProjectLandingPageUrl(project: UrlableProject) {
 
 export function getWorkflowUrl(project: UrlableProject, slug: Workflow["slug"]) {
   return `${getProjectRootUrl(project)}/kanban/${slug}`;
+}
+
+export function getStoryDetailListUrl(project: UrlableProject, storyId: number) {
+  return [`workspace`, project.workspaceId, "project", project.id, "story", storyId];
+}
+
+export function getStoryDetailUrl(project: UrlableProject, storyId: number) {
+  return `/${getStoryDetailListUrl(project, storyId).join("/")}`;
 }
