d92c326b3d15932f27ea51b0d6a8a7ce6b7fddf1
paulguichon <pguichon@amarena.io>
2025-10-02 16:47:35 +0200

    feat: add ui/button

---
+++ src/libs/shared/components/ui/button/button-add.component.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2025 BIRU
+ *
+ * This file is part of Tenzu.
+ *
+ * Tenzu is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * You can contact BIRU at ask@biru.sh
+ *
+ */
+
+import { ChangeDetectionStrategy, Component, input } from "@angular/core";
+import { ButtonComponent } from "./button.component";
+import { IconName, LevelType, ButtonType } from "../ui.types";
+import { ButtonInterface } from "./button.interface";
+import { MatButtonAppearance } from "@angular/material/button";
+
+@Component({
+  selector: "app-button-add",
+  imports: [ButtonComponent],
+  templateUrl: "./button-base.component.html",
+  host: {
+    style: "display: inline-block",
+  },
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ButtonAddComponent implements ButtonInterface {
+  appearance = input<MatButtonAppearance>("filled");
+  level = input<LevelType>("tertiary");
+  translocoKey = input<string>("commons.add");
+  type = input<ButtonType>("button");
+  iconName = input<IconName | undefined>("add");
+  iconOnly = input<boolean>(false);
+  disabled = input<boolean>(false);
+}
