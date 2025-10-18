30d6bc3e2a665a4a71821518b21fc26f8b9a9302
Julie Rymer <jrymer@biru.sh>
2025-10-18 02:00:57 +0200

    feat(comments): real time update of comments

--- src/libs/utils/services/ws/services/apply-event.function.ts
+++ src/libs/utils/services/ws/services/apply-event.function.ts
@@ -30,6 +30,7 @@ import {
   ProjectRoleEventType,
   StoryAssignmentEventType,
   StoryAttachmentEventType,
+  StoryCommentEventType,
   StoryEventType,
   UserEventType,
   WorkflowEventType,
@@ -73,6 +74,7 @@ import { Role } from "@tenzu/repository/membership";
 import { WorkspacePermissions } from "@tenzu/repository/permission/permission.model";
 import { NotFoundEntityError } from "@tenzu/repository/base/errors";
 import { ProjectRoleDetail, ProjectRoleRepositoryService } from "@tenzu/repository/project-roles";
+import { StoryComment, StoryCommentRepositoryService } from "@tenzu/repository/story-comment";
 
 export function applyStoryAssignmentEvent(message: WSResponseEvent<unknown>) {
   const storyService = inject(StoryRepositoryService);
@@ -334,6 +336,31 @@ export function applyStoryAttachmentEvent(message: WSResponseEvent<unknown>) {
         storyAttachmentRepositoryService.deleteEntitySummary(content.attachment.id);
       }
       break;
+    }
+  }
+}
+
+export function applyStoryCommentEvent(message: WSResponseEvent<unknown>) {
+  const storyCommentRepositoryService = inject(StoryCommentRepositoryService);
+  const storyRepositoryService = inject(StoryRepositoryService);
+  const currentStoryDetail = storyRepositoryService.entityDetail();
+  const content = message.event.content as { comment: StoryComment; ref: number };
+  if (currentStoryDetail?.ref === content.ref) {
+    switch (message.event.type) {
+      case StoryCommentEventType.CreateStoryComment: {
+        storyCommentRepositoryService.setEntityDetail(content.comment, { prepend: true });
+        storyRepositoryService.updateCommentsCount(content.ref, 1);
+        break;
+      }
+      // @ts-expect-error FALLS THROUGH
+      case StoryCommentEventType.DeleteStoryComment: {
+        storyRepositoryService.updateCommentsCount(content.ref, -1);
+      }
+      // eslint-disable-next-line no-fallthrough
+      case StoryCommentEventType.UpdateStoryComment: {
+        storyCommentRepositoryService.updateEntitySummary(content.comment.id, content.comment);
+        break;
+      }
     }
   }
 }
